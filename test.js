fe454b3a8c78e22accf834a6624b9dec6a11e6e5
Serenade419 <12npark@gmail.com>
2022-08-17 02:58:39 -0400

    Updated testing information

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const port = 8080;
-const host = "0.0.0.0";
-
-app.use(express.static(__dirname + '/public'));
-console.log(`Listening on port ${port}`);
+console.log("Test Complete");
